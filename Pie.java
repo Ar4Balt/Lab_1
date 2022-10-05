7364a3d84b6ab279e6b2c164eb5c87220317f8e1
Tunnikov Artur <tunnikovartur@gmail.com>
2022-10-05 12:44:08 +0300

    Add class Pie

---
+++ Pie.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Pie extends Food{
+    public Pie(String Filing){
+        super("pie");
+        this.Name = Filing;
+        this.Calories = 45.8f;
+    }
+    public Pie(){
+        super("pie");
+        this.Name = Choice();
+        this.Calories = 45.8f;
+    }
+
+    public String Choice(){
+        System.out.println("What do you prefer green or black? (G/b): ");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String color_Tea = scanner.nextLine().toLowerCase();
+            if (color_Tea.equals("g") ||
+                    color_Tea.equals("gr") ||
+                    color_Tea.equals("green")) {
+                return "green";
+            }
+            if (color_Tea.equals("b") ||
+                    color_Tea.equals("bl") ||
+                    color_Tea.equals("black")) {
+                return "black";
+            } else {
+                System.out.println("Sorry, I don't hear you, please repeat!");
+            }
+        }
+    }
+}
