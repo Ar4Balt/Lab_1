3e70528ab8b27dad9198fdf48ccc4a176736315c
Tunnikov Artur <tunnikovartur@gmail.com>
2022-10-05 12:50:59 +0300

    Remove comments

--- Food.java
+++ Food.java
@@ -32,56 +32,5 @@ public abstract class Food implements Consumable {
     public void consume(){
         System.out.println(this + " has been eaten");
     }
-    /*
-    protected String name = null;
-    protected double colories = 0.f;
 
-    public Food(String name, double colories) {
-        this.name = name;
-        this.colories = colories;
-    }
-
-    public void setColories(double colories) {
-        this.colories = colories;
-    }
-    public double getColories() {
-        return colories;
-    }
-
-    public Food(String name) {
-        this.name = name;
-    }
-
-    public Food(){
-        name = null;
-    }
-
-
-    public String toString() {
-        return "Food{" +
-                "name='" + name + '\'' +
-                '}' + "";
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean equals(Object arg0) {
-        if (!(arg0 instanceof Food)) {
-            return false;
-        }
-        if (name==null || ((Food)arg0).name==null) {
-            return false;
-        }
-        return name.equals(((Food)arg0).name);
-    }
-
-    public void consum() {
-        System.out.println(this + "has been eaten");
-    }
-*/
 }
