b3496c1a118d96a9f705066ff2a8e0efb844a744
Tunnikov Artur <tunnikovartur@gmail.com>
2022-10-05 12:40:44 +0300

    Add and expanded variants choice tea (Green/black)

--- Tea.java
+++ Tea.java
@@ -4,23 +4,28 @@ public class Tea extends Food{
     public Tea(String Color){
         super("tea");
         this.Name = Color;
-        this.Calories = Double.parseDouble(Color);
+        this.Calories = 45.8f;
     }
     public Tea(){
         super("tea");
         this.Name = Choice();
-        this.Calories = Double.parseDouble(Choice());
+        this.Calories = 45.8f;
     }
 
     public String Choice(){
         System.out.println("What do you prefer green or black? (G/b): ");
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            if (scanner.nextLine().toLowerCase().equals("g")) {
-                return scanner.nextLine().toLowerCase();
+            String color_Tea = scanner.nextLine().toLowerCase();
+            if (color_Tea.equals("g") ||
+                    color_Tea.equals("gr") ||
+                    color_Tea.equals("green")) {
+                return "green";
             }
-            if (scanner.nextLine().toLowerCase().equals("b")) {
-                return scanner.nextLine().toLowerCase();
+            if (color_Tea.equals("b") ||
+                    color_Tea.equals("bl") ||
+                    color_Tea.equals("black")) {
+                return "black";
             } else {
                 System.out.println("Sorry, I don't hear you, please repeat!");
             }
